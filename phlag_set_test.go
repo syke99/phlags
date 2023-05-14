db6a0e7eb40a5058477853d7ff7c3780db65cfe4
syke99 <quinnmillican@gmail.com>
2023-05-14 15:02:48 -0600

    phlag-set and tests

---
+++ phlag_set_test.go
@@ -0,0 +1,66 @@
+package phlags
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestNewSet(t *testing.T) {
+	// Act
+	greetingSet := NewSet("greet")
+
+	// Assert
+	assert.NotNil(t, greetingSet)
+
+	// Clean-up
+	pFull = make(map[string]any)
+	pShort = make(map[string]any)
+}
+
+func TestPhlagSet_AddPhlag(t *testing.T) {
+	// Arrange
+	politeSet := NewSet("polite")
+	hello := New("-h", "--hello", "test usage", "test default")
+
+	// Act
+	politeSet.AddPhlag(hello)
+
+	// Assert
+	assert.Equal(t, 1, politeSet.set)
+}
+
+func TestPhlagSet_Parse(t *testing.T) {
+	// Arrange
+	politeSet := NewSet("polite")
+	hello := New("-h", "--hello", "test usage", "test default")
+	goodbye := New("-g", "--goodbye", "test usage", "test default")
+
+	politeSet.AddPhlag(hello).AddPhlag(goodbye)
+
+	questionSet := NewSet("questions")
+	who := New("-wh", "--who", "test usage", "test default")
+	what := New("-wt", "--what", "test usage", "test default")
+
+	questionSet.AddPhlag(who).AddPhlag(what)
+
+	os.Args = []string{"", "polite", "-h=helloWorld", "1", "2", "3", "-g=goodbyeAll", "4", "5", "6"}
+
+	// Act
+	err := Parse()
+
+	// Assert
+	assert.NoError(t, err)
+	assert.Equal(t, "helloWorld", *hello.String())
+	assert.Equal(t, 1, *hello.args[0].Int())
+	assert.Equal(t, 2, *hello.args[1].Int())
+	assert.Equal(t, 3, *hello.args[2].Int())
+	assert.Equal(t, "goodbyeAll", *goodbye.String())
+	assert.Equal(t, 4, *goodbye.args[0].Int())
+	assert.Equal(t, 5, *goodbye.args[1].Int())
+	assert.Equal(t, 6, *goodbye.args[2].Int())
+
+	// Clean-up
+	pFull = make(map[string]any)
+	pShort = make(map[string]any)
+}
---
+++ phlag_test.go
@@ -0,0 +1,57 @@
+package phlags
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	// Act
+	flg := New("-h", "--hello", "test usage", "test default")
+
+	// Assert
+	assert.NotNil(t, flg)
+
+	// Clean-up
+	pFull = make(map[string]any)
+	pShort = make(map[string]any)
+}
+
+func TestNew_MissingNames(t *testing.T) {
+	// Act
+	flg := New("", "", "test usage", "test default")
+
+	// Assert
+	assert.Nil(t, flg)
+
+	// Clean-up
+	pFull = make(map[string]any)
+	pShort = make(map[string]any)
+}
+
+func TestParse(t *testing.T) {
+	// Arrange
+	hello := New("-h", "--hello", "test usage", "test default")
+	goodbye := New("-g", "--goodbye", "test usage", "test default")
+
+	os.Args = []string{"", "-h=helloWorld", "1", "2", "3", "-g=goodbyeAll", "4", "5", "6"}
+
+	// Act
+	err := Parse()
+
+	// Assert
+	assert.NoError(t, err)
+	assert.Equal(t, "helloWorld", *hello.String())
+	assert.Equal(t, 1, *hello.args[0].Int())
+	assert.Equal(t, 2, *hello.args[1].Int())
+	assert.Equal(t, 3, *hello.args[2].Int())
+	assert.Equal(t, "goodbyeAll", *goodbye.String())
+	assert.Equal(t, 4, *goodbye.args[0].Int())
+	assert.Equal(t, 5, *goodbye.args[1].Int())
+	assert.Equal(t, 6, *goodbye.args[2].Int())
+
+	// Clean-up
+	pFull = make(map[string]any)
+	pShort = make(map[string]any)
+}
